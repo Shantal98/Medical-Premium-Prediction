b4f65ed61f3265e4c0acb02af44672c702268f08
Shantal98 <117507867+Shantal98@users.noreply.github.com>
2025-04-27 22:05:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ X = X.drop(columns=['Height', 'Weight'])
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# --- Model Training (only first time or on update) ---
+# --- Model Training  ---
 @st.cache_resource
 def train_model():
     model = RandomForestRegressor(n_estimators=100, random_state=42)
